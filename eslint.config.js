1e642e3c007351ebbbadc1e71f30b9813ce4c738
JW <jiangweixian1994@gmail.com>
2024-02-04 10:48:24 +0800

    :rotating_light:  ignore index.d.ts

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,6 @@ const { aiou } = require('@aiou/eslint-config')
 
 module.exports = aiou({ ssr: false }, [
   {
-    ignores: ['index.js', '**/fixtures/**'],
+    ignores: ['index.js', 'index.d.ts', '**/fixtures/**'],
   },
 ])
